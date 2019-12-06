161dd879a291a9280c163320c1a591a5e61662c8
Gayasuddin Siddiqui <gayasuddin93@gmail.com>
2019-12-07 05:17:15 +0530

    first commit

---
+++ precache-manifest.e8879df4240f6fe509a397caac094e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1823da2568c7266f398bbbf277e45c3b",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "720f2117c81e9e3ed09a",
+    "url": "/monster-dex/static/css/main.3a4bc4f2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-dex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-dex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "720f2117c81e9e3ed09a",
+    "url": "/monster-dex/static/js/main.074c2bbd.chunk.js"
+  },
+  {
+    "revision": "f4b0002adf6a84965cad",
+    "url": "/monster-dex/static/js/runtime-main.22fe40c7.js"
+  }
+]);
